8cd73069a14c61808822def0711a772109526be0
itamarseg <itamar@seccosquared.com>
2024-10-07 15:20:02 -0400

    Update Signup Button

--- dist/home-page.js
+++ dist/home-page.js
@@ -1,2 +1,2 @@
-(()=>{document.getElementById("signup-button").addEventListener("click",function(t){t.preventDefault(),window.location.href="https://www.pimuy.com/TPL7P8MS/XK5NQPL4/"});})();
+(()=>{document.getElementById("signup-button").addEventListener("click",function(t){t.preventDefault(),window.location.href="https://bgtracking.com/click/14"});})();
 //# sourceMappingURL=home-page.js.map
